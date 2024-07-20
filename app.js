47a5c65830f4881769adb00531cb3b44b7645a75
ilovecrayons <huangfeiyang2008@gmail.com>
2024-07-20 16:30:31 -0700

    har industries

--- app.js
+++ app.js
@@ -7,3 +7,11 @@ function closeBar(){
     // document.getElementById("navBar").style.width = "0";
     document.getElementById("navBar").classList.remove("open");
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('http://localhost:5000/scare')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('textbox').textContent = data.message; 
+        })
+});
