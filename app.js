1b83aaeb42c2c2654607874974285116044a1053
ilovecrayons <huangfeiyang2008@gmail.com>
2024-07-20 17:07:47 -0700

    a

--- app.js
+++ app.js
@@ -12,6 +12,6 @@ document.addEventListener('DOMContentLoaded', function() {
     fetch('http://localhost:5000/scare')
         .then(response => response.json())
         .then(data => {
-            document.getElementById('textbox').textContent = data.message; 
+            document.getElementById('textbox').textContent = data.response; 
         })
 });
