e1a858d25b2f142b7b4f50eadc3846055cd22509
datboi212 <datboi212mc@gmail.com>
2024-07-21 13:39:19 -0700

    final commit

--- locator.js
+++ locator.js
@@ -19,3 +19,13 @@ function get(url) {
 function getNear() {
     get(`/leoLianLocator?name=${logInName}`)
 }
+
+function openBar(){
+    // document.getElementById("navBar").style.width = "250px";
+    document.getElementById("navBar").classList.add("open");
+}
+
+function closeBar(){
+    // document.getElementById("navBar").style.width = "0";
+    document.getElementById("navBar").classList.remove("open");
+}
